e94ac8aa4a7a5e682b88a9eaa64d9ebe255776ce
sibo-t <stmsabala@gmail.com>
2022-12-04 15:48:07 +0200

    Can detect face on image

--- cam_facial_detection.py
+++ cam_facial_detection.py
@@ -48,7 +48,7 @@ def main():
     cam = create_capture(video_src, fallback='synth:bg={}:noise=0.05'.format(cv.samples.findFile('images/faces/face001.jpg')))
 
     while True:
-        img = cv.imread("images/me.jpeg", cv.IMREAD_COLOR)
+        _ret, img = cam.read()
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         gray = cv.equalizeHist(gray)
 
